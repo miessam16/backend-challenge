ea640bd16d83f521c3501430ad5941e0473ef6c8
Mohamed Essam <miessam16@gmail.com>
2019-10-05 12:03:31 +0200

    Remove unwanted logs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import {NotifyModule} from "./notify/notify.module";
 import { MongooseModule } from '@nestjs/mongoose';
-console.log(process.env.MONGO_URL);
+
 @Module({
   imports: [
       MongooseModule.forRoot(process.env.MONGO_URL, {useNewUrlParser: true}),
