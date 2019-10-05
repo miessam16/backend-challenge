35c5f7c1fb57cf362d9a4738c15652fbb81f1e85
Mohamed Essam <miessam16@gmail.com>
2019-10-05 10:31:07 +0200

    Connect to mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
+import {NotifyModule} from "./notify/notify.module";
+import { MongooseModule } from '@nestjs/mongoose';
+console.log(process.env.MONGO_URL);
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+      MongooseModule.forRoot(process.env.MONGO_URL, {useNewUrlParser: true}),
+      NotifyModule
+  ],
 })
 export class AppModule {}
